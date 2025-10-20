96aab445878ddd5d365bbddd2eb6c0a37ed6b672
Voronskoy <marskoy27@gmail.com>
2025-10-20 17:07:33 +0300

    fixpos

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const fruits = document.querySelectorAll(".buttons > button");
 let activeFruit = null;
 const area = document.getElementById("area");
+let x = 0;
+let y = 0;
 console.dir(area);
 for (const fruit of fruits) {
-  fruit.addEventListener("mousedown", () => {
+  fruit.addEventListener("mousedown", (event) => {
     activeFruit = fruit;
+    x = event.offsetX;
+    y = event.offsetY;
     fruit.style.cursor = "move";
   });
   fruit.addEventListener("mousemove", (event) => {
     if (activeFruit) {
       console.log(event);
-      activeFruit.style.left = event.clientX - area.offsetLeft + "px";
-      activeFruit.style.top = event.clientY - area.offsetTop + "px";
+      activeFruit.style.left = event.clientX - area.offsetLeft - x + "px";
+      activeFruit.style.top = event.clientY - area.offsetTop - y + "px";
     }
   });
   fruit.addEventListener("mouseup", () => {
